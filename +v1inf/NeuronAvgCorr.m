2672d6b4863d981bc860dedec2758698f82e1fc6
Selmaan <chettih@fas.harvard.edu>
2018-05-18 15:12:41 -0400

    update neurAvgCorr with uncentered data
    same changes as previously implemented for the targAvgCorr table

--- +v1inf/NeuronAvgCorr.m
+++ +v1inf/NeuronAvgCorr.m
@@ -6,6 +6,8 @@ neur_avg_corr=NULL: float # Average Naive/Trace Correlation with other neurons
 neur_std_corr=NULL: float # Std of Naive/Trace Correlation with other neurons
 neur_avg_inf: float       # Average influence of this neuron
 neur_std_inf: float       # Std influence of this neuron
+neur_avg_inf_raw: float       # Average influence of this neuron using uncentered influence
+neur_std_inf_raw: float       # Std influence of this neuron using uncentered influence
 %}
 
 classdef NeuronAvgCorr < dj.Imported
@@ -19,8 +21,11 @@ classdef NeuronAvgCorr < dj.Imported
             validFilt = v1inf.FiltOverlap - 'filt_overlap>0';
             validStim = proj(v1inf.Target & (v1inf.Influence & 'inf_shuf_p>5'));
             validInf = (v1inf.Influence & key) & 'inf_dist>25';
+            validInfRaw = (v1inf.InfVals & key) & 'inf_dist>25';
             [iC, sN, nID] = fetchn((validInf & validStim) & validFilt,...
                 'inf_naivecorr','inf_shuf_n','neur_id','ORDER BY neur_id');
+            [sN_raw,nID_raw] = fetchn(validInfRaw & validFilt,...
+                'inf_n_raw','neur_id','ORDER BY neur_id');
             allNeur = unique(nID);
             keys = repmat(key,length(allNeur),1);
             for i = 1:length(allNeur)
@@ -30,6 +35,9 @@ classdef NeuronAvgCorr < dj.Imported
                 keys(i).neur_std_corr = nanstd(iC(neurInd));
                 keys(i).neur_avg_inf = nanmean(sN(neurInd));
                 keys(i).neur_std_inf = nanstd(sN(neurInd));
+                neurIndRaw = nID_raw == allNeur(i);
+                keys(i).neur_avg_inf_raw = nanmean(sN_raw(neurIndRaw));
+                keys(i).neur_std_inf_raw = nanstd(sN_raw(neurIndRaw));
             end
             self.insert(keys(:)),
         end
