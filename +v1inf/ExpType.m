cc244ba4e99cd0bc43aa13ed594c630eb8134095
Selmaan <smaaan@gmail.com>
2018-05-22 11:31:28 -0400

    update table description

--- +v1inf/ExpType.m
+++ +v1inf/ExpType.m
@@ -10,7 +10,7 @@ end
 
 
 %% Experiment Type listing
-% Experiments w/ mice 18-29 are all single-contrast
+% Experiments w/ mice 18-29 are all single-contrast (except for two in m26)
 % Experiments w/ mouse 33 are single-contrast-dense 
 % Experiments w/ mouse 37 from 12-13 to 12-27  are monitor-off (does not include those in 2018)
 % Experiments w/ mouse 37 in 2018 and mice 38-41 are multi-contrast
